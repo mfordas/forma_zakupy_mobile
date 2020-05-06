528a009957da29a3ac73b8f498cb42a02587aa3b
Mateusz <mateusz.fordas@gmail.com>
2020-05-06 14:58:16 +0200

    Project in react-native created

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>FormaZakupy Mobile app!</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
